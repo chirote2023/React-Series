e4df5e523ada33c790e026be8ca3c7b400bdfaaf
Piyush chirote <134203686+chirote2023@users.noreply.github.com>
2024-08-24 15:17:17 +0530

    Redux React

---
+++ 11 Redux-React/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import AddTodo from './components/addtodo'
+import Todo from './components/todo'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Learn Redux in React</h1>
+      <AddTodo />
+      <Todo />
+    </>
+  )
+}
+
+export default App
